3efa22b650e3d5e2b525ed910ed7a810a509d510
Caro HULOT <130963329+CaroHP24@users.noreply.github.com>
2025-04-09 12:09:10 +0200

    Test + resultat

--- test.py
+++ test.py
@@ -1,33 +1,29 @@
 import time
 import random
 
-start1=time.time()
+start=time.time()
 
 total=0
 for i in range(1000):
     for j in range(1000):
             total += (i * j + random.randint(0, 10))
 
-end1 = time.time()
+end = time.time()
+
+print(f"Résultat final : {total}")
+print(f"Temps d'exécution : {end - start:.2f} secondes")
 
 
 
-
-
-start2 = time.time()
+start = time.time()
 
 with open("FichierTest.txt", "r", encoding="utf-8") as f:
     contenu = f.read()
 
-print(contenu)
-end2 = time.time()
-
-
-print(f"Résultat final : {total}")
-print(f"Temps d'exécution : {end1 - start1:.2f} secondes")
+end = time.time()
 
 print(f"Lecture terminée. Taille : {len(contenu)} caractères")
-print(f"Temps de lecture : {end2 - start2:.2f} secondes")
+print(f"Temps de lecture : {end - start:.2f} secondes")
 
 
 
